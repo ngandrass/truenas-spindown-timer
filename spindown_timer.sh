9036db4b5ba5d5a9a6565beaf6e0b60ebfa0d2f7
ngandrass <ngandrass@squacu.de>
2019-06-22 15:06:36 +0200

    Add documentation header to script

--- spindown_timer.sh
+++ spindown_timer.sh
@@ -1,6 +1,36 @@
 #!/usr/bin/env bash
 
-TIMEOUT=3600       # Default number of seconds to wait for I/O before considering a drive as idle
+# ##################################################
+# FreeNAS HDD Spindown Timer
+# Monitors drive I/O and forces HDD spindown after a given idle period.
+#
+# See: https://github.com/ngandrass/freenas-spindown-timer
+#
+#
+# MIT License
+# 
+# Copyright (c) 2019 Niels Gandraß
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ##################################################
+
+TIMEOUT=3600       # Default timeout before considering a drive as idle
 IGNORED_DRIVES=""  # Default list of drives that are never spun down
 VERBOSE=1          # Default verbosity level
 DRYRUN=0           # Default for dryrun option
